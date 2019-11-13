5ee7c777ea015f1ac72ceaba71219f44ba60474a
PenghaiZhang <47203811+PenghaiZhang@users.noreply.github.com>
2019-11-14 09:07:16 +1100

    Support running the launcher from different directories (#27)
    Also fix the issue of failing to launch from directories containing spaces.

--- launcher-scripts/Windows-launcher.bat
+++ launcher-scripts/Windows-launcher.bat
@@ -1,6 +1,6 @@
 @echo off
 set OLD_JAVA_HOME=%JAVA_HOME%
 set JAVA_HOME=%cd%\jdk8u212-b03-jre
-start "Admin console launcher" /D "%cd%\libs" %JAVA_HOME%\bin\javaw -DLAUNCHER_JAVA_PATH="%JAVA_HOME%\bin\java" -jar admin.jar
+start "Admin console launcher" /D "%cd%\libs" "%JAVA_HOME%\bin\javaw" -DLAUNCHER_JAVA_PATH="%JAVA_HOME%\bin\java" -jar admin.jar
 set JAVA_HOME=%OLD_JAVA_HOME%
 set OLD_JAVA_HOME=
