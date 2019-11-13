145b6c4e2a73245fad8057c4b44e89ae69ff1936
Penghai <penghai.zhang@edalex.com>
2019-11-13 17:00:53 +1100

    Use 'readlink' instead of 'cd' Include the changes in 'Mac-launcher.sh'

--- launcher-scripts/Linux-launcher.sh
+++ launcher-scripts/Linux-launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OLD_JAVA_HOME=$JAVA_HOME
-SCRIPT_DIRECTORY=`(cd $(dirname $0); pwd)`
+SCRIPT_DIRECTORY=`readlink -f "$(dirname "$0")"`
 export JAVA_HOME=$SCRIPT_DIRECTORY/jdk8u212-b03-jre
 "$JAVA_HOME/bin/java" -DLAUNCHER_JAVA_PATH="$JAVA_HOME/bin/java" -jar "$SCRIPT_DIRECTORY/libs/admin.jar"
 export JAVA_HOME=$OLD_JAVA_HOME
