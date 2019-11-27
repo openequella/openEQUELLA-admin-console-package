03e46fd2737c694a72deeed6cae2583a9afdb3a8
Penghai <penghai.zhang@edalex.com>
2019-11-27 16:15:09 +1100

    Use '%~dp0' to get the correct path to the launcher

--- launcher-scripts/Windows-launcher.bat
+++ launcher-scripts/Windows-launcher.bat
@@ -1,6 +1,7 @@
 @echo off
 set OLD_JAVA_HOME=%JAVA_HOME%
-set JAVA_HOME=%cd%\jdk8u212-b03-jre
-start "Admin console launcher" /D "%cd%\libs" "%JAVA_HOME%\bin\javaw" -DLAUNCHER_JAVA_PATH="%JAVA_HOME%\bin\java" -jar admin.jar
+set BAT_DIRECTORY=%~dp0
+set JAVA_HOME=%BAT_DIRECTORY%jdk8u212-b03-jre
+start "Admin console launcher" /D "%BAT_DIRECTORY%\libs" "%JAVA_HOME%\bin\javaw" -DLAUNCHER_JAVA_PATH="%JAVA_HOME%\bin\java" -jar admin.jar
 set JAVA_HOME=%OLD_JAVA_HOME%
 set OLD_JAVA_HOME=
