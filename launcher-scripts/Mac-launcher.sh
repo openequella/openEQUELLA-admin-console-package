6ab77f65692b4a9f09f35faac2e383dd2fea2847
PenghaiZhang <47203811+PenghaiZhang@users.noreply.github.com>
2019-12-02 11:00:22 +1100

    Update version to 1.0.1
    * Fix the issue of failing to launch from directories containing spaces.
    * Support running the launcher from different directories
    * Use Git Release tool to publish artifacts to Github

--- launcher-scripts/Mac-launcher.sh
+++ launcher-scripts/Mac-launcher.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 OLD_JAVA_HOME=$JAVA_HOME
-export JAVA_HOME=`pwd`/jdk8u212-b03-jre/Contents/Home
-"$JAVA_HOME/bin/java" -DLAUNCHER_JAVA_PATH="$JAVA_HOME/bin/java" -jar ./libs/admin.jar
+SCRIPT_DIRECTORY=`readlink -f "$(dirname "$0")"`
+export JAVA_HOME=$SCRIPT_DIRECTORY/jdk8u212-b03-jre/Contents/Home
+"$JAVA_HOME/bin/java" -DLAUNCHER_JAVA_PATH="$JAVA_HOME/bin/java" -jar "$SCRIPT_DIRECTORY/libs/admin.jar"
 export JAVA_HOME=$OLD_JAVA_HOME
