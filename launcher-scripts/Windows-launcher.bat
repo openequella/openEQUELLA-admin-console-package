bdc49488807a3bee0fd9ec43235512e3feaa2b72
Samantha Fisher <samantha.fisher@edalex.com>
2021-05-10 16:10:47 +1000

    Fix up quotes for set commands

--- launcher-scripts/Windows-launcher.bat
+++ launcher-scripts/Windows-launcher.bat
@@ -1,7 +1,7 @@
 @echo off
-set "OLD_JAVA_HOME=%JAVA_HOME%"
+set OLD_JAVA_HOME="%JAVA_HOME%"
 set "BAT_DIRECTORY=%~dp0"
 set "JAVA_HOME=%BAT_DIRECTORY%jdk8u242-b08-jre"
 start "Admin console launcher" /D "%BAT_DIRECTORY%\libs" "%JAVA_HOME%\bin\javaw" -DLAUNCHER_JAVA_PATH="%JAVA_HOME%\bin\java" -jar admin.jar
-set "JAVA_HOME=%OLD_JAVA_HOME%"
-set "OLD_JAVA_HOME="
+set JAVA_HOME="%OLD_JAVA_HOME%"
+set OLD_JAVA_HOME=""
