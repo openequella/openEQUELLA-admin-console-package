1510e58af793860f8531d623bb3fe3e65c949002
Penghai <penghai.zhang@edalex.com>
2023-02-07 15:05:03 +1100

    feat: update the bundled JRE to v11
    The OpenJDK distribution is Temurin as it's the chose distribution in OEQ

--- launcher-scripts/Linux-launcher.sh
+++ launcher-scripts/Linux-launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OLD_JAVA_HOME=$JAVA_HOME
 SCRIPT_DIRECTORY=`readlink -f "$(dirname "$0")"`
-export JAVA_HOME=$SCRIPT_DIRECTORY/jdk8u242-b08-jre
+export JAVA_HOME=$SCRIPT_DIRECTORY/jdk-11.0.18+10-jre
 "$JAVA_HOME/bin/java" -DLAUNCHER_JAVA_PATH="$JAVA_HOME/bin/java" -jar "$SCRIPT_DIRECTORY/libs/admin.jar"
 export JAVA_HOME=$OLD_JAVA_HOME
