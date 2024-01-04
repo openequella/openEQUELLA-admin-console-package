4b1d2d7ff143279374e45da1e7ffda832791f24f
Penghai <penghai.zhang@edalex.com>
2024-01-04 15:59:33 +1100

    feat: update the bundled JRE to V21

--- launcher-scripts/Linux-launcher.sh
+++ launcher-scripts/Linux-launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OLD_JAVA_HOME=$JAVA_HOME
 SCRIPT_DIRECTORY=`readlink -f "$(dirname "$0")"`
-export JAVA_HOME=$SCRIPT_DIRECTORY/jdk-11.0.18+10-jre
+export JAVA_HOME=$SCRIPT_DIRECTORY/jdk-21.0.1+12-jre
 "$JAVA_HOME/bin/java" -DLAUNCHER_JAVA_PATH="$JAVA_HOME/bin/java" -jar "$SCRIPT_DIRECTORY/libs/admin.jar"
 export JAVA_HOME=$OLD_JAVA_HOME
